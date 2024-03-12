b3907f89655d4d174fc60eb19a78776df2964422
Tilson Mateus <tilsonmat@gmail.com>
2024-03-12 16:46:35 +0100

    CRM-1: Add mery-ui +akage for easyle create the front end

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,18 @@ import typography from '@tailwindcss/typography'
 
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
+		"./resources/**/*.blade.php",
+		 "./resources/**/*.js",
+		 "./resources/**/*.vue",
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
   theme: {
     extend: {},
   },
   plugins: [
-    form,
-    typography
-  ],
+		form,
+		typography,
+		require("daisyui")
+	],
 }
 
